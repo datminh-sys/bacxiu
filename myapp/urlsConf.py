0c4fe9fe32470cf8a01c2b5b1c3f95fb0b3a753d
datminh-sys <haminhdat1512@gmail.com>
2026-04-03 21:34:30 +0700

    s2

--- myapp/urlsConf.py
+++ myapp/urlsConf.py
@@ -7,9 +7,9 @@ urlpatterns = [
     path('home', views.home, name='home'),
 
     # Quản lý sản phẩm
-    path('products', views.show_products, name='products'),
-    path('add_product', views.add_product, name='add_product'),
-    path('list_products', views.list_products, name='list_products'),
+    path('products/', views.list_products, name='list_products'),
+    path('add_product/', views.add_product, name='add_product'),
+    path('list_products/', views.list_products, name='list_products'),
     path('edit_product/<int:product_id>/', views.edit_product, name='edit_product'),
     path('delete_product/<int:product_id>/', views.delete_product, name='delete_product'),
     path('new_product', views.new_product, name='new_product'),
